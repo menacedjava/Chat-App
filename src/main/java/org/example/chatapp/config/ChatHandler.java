7595cd0d21545e6e39254ea8bb3775a966eaac7b
Madumarov Davron <menaceteacher@gmail.com>
2025-03-15 20:58:25 +0500

    add

--- src/main/java/org/example/chatapp/config/ChatHandler.java
+++ src/main/java/org/example/chatapp/config/ChatHandler.java
@@ -1,8 +1,34 @@
 package org.example.chatapp.config;
 
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-public class ChatHandler extends TextWebSocketHandler {
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
+public class ChatHandler extends TextWebSocketHandler {
+    private static final Set<WebSocketSession> sessions = Collections.synchronizedSet(new HashSet<>());
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        sessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+        for (WebSocketSession webSocketSession : sessions) {
+            if (webSocketSession.isOpen()) {
+                webSocketSession.sendMessage(message);
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, org.springframework.web.socket.CloseStatus status) {
+        sessions.remove(session);
+    }
 }
