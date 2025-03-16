4d442d416b8589fbdc31d31728777550d5a0db75
Madumarov Davron <menaceteacher@gmail.com>
2025-03-17 02:25:27 +0500

    add

--- src/main/java/org/example/chatapp/model/ChatMessage.java
+++ src/main/java/org/example/chatapp/model/ChatMessage.java
@@ -1,4 +1,13 @@
 package org.example.chatapp.model;
 
 public class ChatMessage {
+    private String sender;
+    private String content;
+    private String timestamp;
+
+    public ChatMessage(String sender, String content, String timestamp) {
+        this.sender = sender;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
 }
