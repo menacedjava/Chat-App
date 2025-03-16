58d1b1e3f51eb84840c9188472d7d485da6afc40
Madumarov Davron <menaceteacher@gmail.com>
2025-03-17 02:32:15 +0500

    add

--- src/main/java/org/example/chatapp/controller/ChatController.java
+++ src/main/java/org/example/chatapp/controller/ChatController.java
@@ -1,5 +1,6 @@
 package org.example.chatapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/chat")
 public class ChatController {
 
+    @Autowired
+    private ChatController chatController;
     @GetMapping("/test")
     public String testEndpoint() {
         return "Chat server is running!";
     }
+
 }
--- src/main/java/org/example/chatapp/wevsocket/ChatWebSocketHandler.java
+++
@@ -1,6 +0,0 @@
-package org.example.chatapp.wevsocket;
-
-import org.springframework.web.socket.handler.TextWebSocketHandler;
-
-public class ChatWebSocketHandler extends TextWebSocketHandler {
-}
