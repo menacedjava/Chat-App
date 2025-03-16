8ce6e1c583800be59cb22cd6b076cc169ea9b962
Madumarov Davron <menaceteacher@gmail.com>
2025-03-17 02:27:23 +0500

    add

--- src/main/java/org/example/chatapp/model/ChatMessage.java
+++ src/main/java/org/example/chatapp/model/ChatMessage.java
@@ -10,4 +10,28 @@ public class ChatMessage {
         this.content = content;
         this.timestamp = timestamp;
     }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
 }
