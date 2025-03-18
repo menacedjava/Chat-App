8d89c55f3cb847bbb8b1c0f5a0f30f37e70c8275
Madumarov Davron <menaceteacher@gmail.com>
2025-03-18 12:34:01 +0500

    add

--- src/main/java/org/example/chatapp/controller/TypingController.java
+++ src/main/java/org/example/chatapp/controller/TypingController.java
@@ -1,4 +1,15 @@
 package org.example.chatapp.controller;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class TypingController {
+
+    @MessageMapping("/typing")
+    @SendTo("/topic/typing")
+    public String handleTyping(String username) {
+        return username + " is typing...";
+    }
 }
